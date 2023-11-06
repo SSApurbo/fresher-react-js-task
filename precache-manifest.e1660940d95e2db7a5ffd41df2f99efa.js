97e482a233b3b22c0fa648bd41b517f82499136d
shadman126 <shadman.shakib059@gmail.com>
2023-11-06 22:24:39 +0600

    Updates

---
+++ precache-manifest.e1660940d95e2db7a5ffd41df2f99efa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a09f1237fdc7102cd6a0ae3d0b00d3",
+    "url": "/fresher-react-js-task/index.html"
+  },
+  {
+    "revision": "b89b79e279df3f62d5ac",
+    "url": "/fresher-react-js-task/static/css/main.3bca6fdc.chunk.css"
+  },
+  {
+    "revision": "f23dc4f50844704e9727",
+    "url": "/fresher-react-js-task/static/js/2.81648295.chunk.js"
+  },
+  {
+    "revision": "6a2a939c6c472481a460",
+    "url": "/fresher-react-js-task/static/js/3.5a11cc32.chunk.js"
+  },
+  {
+    "revision": "b89b79e279df3f62d5ac",
+    "url": "/fresher-react-js-task/static/js/main.50a80ce4.chunk.js"
+  },
+  {
+    "revision": "81d7634706549563ac08",
+    "url": "/fresher-react-js-task/static/js/runtime~main.c8882767.js"
+  }
+]);
